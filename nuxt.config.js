a820d1969419c64647ed276362c428c4a1785270
Akatsuki1910 <35941346+Akatsuki1910@users.noreply.github.com>
2021-07-19 17:59:38 +0900

    Revert "Update nuxt.config.js"
    This reverts commit 0ab3ba46c6d66b2b79813a22504fa03d00844fa9.

--- nuxt.config.js
+++ nuxt.config.js
@@ -3,7 +3,7 @@ const fibers = require('fibers')
 
 export default {
   // Disable server-side rendering: https://go.nuxtjs.dev/ssr-mode
-  ssr: true,
+  ssr: false,
 
   // Global page headers: https://go.nuxtjs.dev/config-head
   head: {
